08524dea1ef9fde4e1595e6df4d500a2c8766285
tgray017 <tgray017@gmail.com>
2019-02-16 22:54:03 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,11 @@
 class Show < ActiveRecord::Base
+
+  def self.highest_rating
+    self.maximum(:rating)
+  end
+  
+  def self.most_popular_show
+    self.find_by(:rating => highest_rating)
+  end
+
 end
