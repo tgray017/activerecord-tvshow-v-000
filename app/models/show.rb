2e1be90db4dd7c52b523492a076543615da080d9
tgray017 <tgray017@gmail.com>
2019-02-16 22:58:20 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,19 +1,19 @@
 class Show < ActiveRecord::Base
 
   def self.highest_rating
-    self.maximum(:rating)
+    maximum(:rating)
   end
   
   def self.most_popular_show
-    self.find_by(:rating => highest_rating)
+    find_by(:rating => highest_rating)
   end
   
   def self.lowest_rating
-    self.minimum(:rating)
+    minimum(:rating)
   end
   
   def self.least_popular_show
-    self.find_by(:rating => lowest_rating)
+    find_by(:rating => lowest_rating)
   end
   
   def self.ratings_sum
